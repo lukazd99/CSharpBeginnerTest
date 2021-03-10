3e6f6eeee675e086ad8805dfe9476ef2b76de8e2
lukazd99 <lukazd99@outlook.com>
2021-03-10 10:07:55 +0100

    Success on first test to read content from text file

--- FormQuiz.cs
+++ FormQuiz.cs
@@ -18,7 +18,38 @@ namespace CSharpBeginnerTest
         {
             InitializeComponent();
 
-            
+            readFile();
+        }
+
+
+        //Testing file reading
+        private void readFile()
+        {
+            string line;
+            StreamReader sr = null;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                sr = new StreamReader(@"C:\Work\C# Lecturing\CSharpBeginnerTest\Questions.txt");
+                //Read the first line of text
+                line = sr.ReadLine();
+                //Continue to read until you reach end of file
+                //close the file
+                sr.Close();
+                MessageBox.Show(line);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception: \n" + e.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Executing finally block.");
+                if(sr != null)
+                {
+                    sr.Close();
+                }
+            }
         }
 
         private void btnSubmitAnswer_Click(object sender, EventArgs e)
